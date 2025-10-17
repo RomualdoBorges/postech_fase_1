c55ee13047490f1662dd31769412f6b22550232b
Romualdo Borges <romualdobssilva@gmail.com>
2025-10-17 19:13:09 -0300

    adicionando o componente menu no AppBar

--- src/app/components/AppBar/index.tsx
+++ src/app/components/AppBar/index.tsx
@@ -1,15 +1,26 @@
+"use client";
+
 import React from "react";
 import styles from "./AppBar.module.css";
 import { Box, IconButton, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import Menu from "../Menu";
+import { nav } from "@/app/utils/navData";
+import CloseIcon from "@mui/icons-material/Close";
 
 const AppBar: React.FC = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  function handleClick() {
+    setOpen(!open);
+  }
+
   return (
     <header className={styles.header}>
       <Box className={styles.container}>
         <Box>
-          <IconButton className={styles.menu}>
+          <IconButton className={styles.menu} onClick={handleClick}>
             <MenuIcon fontSize="large" />
           </IconButton>
         </Box>
@@ -21,6 +32,15 @@ const AppBar: React.FC = () => {
           </IconButton>
         </Box>
       </Box>
+
+      {open && (
+        <Box className={styles.nav}>
+          <IconButton onClick={() => setOpen(!open)}>
+            <CloseIcon />
+          </IconButton>
+          <Menu orientation="vertical" navData={nav} />
+        </Box>
+      )}
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Menu from "./components/Menu";
-import { nav } from "./utils/navData";
+// import Menu from "./components/Menu";
+// import { nav } from "./utils/navData";
 
 export default function Home() {
   return (
     <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
-      <Menu orientation="vertical" navData={nav} />
+      {/* <Menu orientation="vertical" navData={nav} /> */}
     </div>
   );
 }
