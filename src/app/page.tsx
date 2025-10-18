a32f632f82a64f4951013e4cb824e6922f8de9d6
Romualdo Borges <romualdobssilva@gmail.com>
2025-10-18 17:59:53 -0300

    montagem da pagina home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,44 @@
 "use client";
 
 import React from "react";
+import styles from "./styles/page.module.css";
 import NewTransaction from "./components/NewTransaction";
-// import NewTransactionForm from "./components/NewTransactionForm";
-// import Menu from "./components/Menu";
-// import { nav } from "./utils/navData";
-// import Balance from "./components/Balance";
-// import BankStatementList, {
-//   BankStatementData,
-// } from "./components/BankStatementList";
+import Menu from "./components/Menu";
+import { nav } from "./utils/navData";
+import Balance from "./components/Balance";
+import BankStatementList, {
+  BankStatementData,
+} from "./components/BankStatementList";
+import { useMediaQuery } from "@mui/material";
 
 export default function Home() {
-  // const bankStatementData: BankStatementData[] = [
-  //   { id: 0, date: "2022-10-18", type: "Depósito", value: 150 },
-  //   { id: 1, date: "2022-11-21", type: "Depósito", value: 100 },
-  //   { id: 2, date: "2022-11-21", type: "Depósito", value: 50 },
-  //   { id: 3, date: "2022-12-21", type: "Transferência", value: -500 },
-  // ];
+  const isMobile = useMediaQuery("(max-width:879px)");
+
+  const bankStatementData: BankStatementData[] = [
+    { id: 0, date: "2022-10-18", type: "Depósito", value: 150 },
+    { id: 1, date: "2022-11-21", type: "Depósito", value: 100 },
+    { id: 2, date: "2022-11-21", type: "Depósito", value: 50 },
+    { id: 3, date: "2022-12-21", type: "Transferência", value: -500 },
+  ];
 
   return (
-    <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
-      {/* <Menu orientation="vertical" navData={nav} /> */}
-      {/* <Balance /> */}
-      {/* <BankStatementList data={bankStatementData} /> */}
+    <div className={styles.container}>
+      <section className={styles.menu}>
+        <Menu
+          orientation={isMobile ? "horizontal" : "vertical"}
+          navData={nav}
+        />
+      </section>
 
-      {/* <NewTransactionForm /> */}
-      <NewTransaction />
+      <section className={styles.cardsCenter}>
+        <Balance />
+        <NewTransaction />
+      </section>
+
+      <section className={styles.statement}>
+        <p className={styles.titleStatement}>Extrato</p>
+        <BankStatementList data={bankStatementData} />
+      </section>
     </div>
   );
 }
